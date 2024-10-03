b1474ef104458ed8214a502446991a7072474cc1
luiz <141868804+tug777@users.noreply.github.com>
2024-10-03 16:43:20 -0300

    1

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'pokemon',
+    'qual primeiro pokemon do red?',
+    'O poliwhirl'
+)
+
+criaCartao(
+    'master duel',
+    'Qual o deck top 1 atual ?',
+    'yubel'
+)
+
+criaCartao(
+    'pokemon',
+    'quem é o deus pokemon ?',
+    'arceus'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
